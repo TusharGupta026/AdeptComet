fa545382626ca96756ac4b62f8c94295f40de3bf
Tushar Gupta <guptatushar802@gmail.com>
2021-10-19 17:49:58 +0530

    first commit

---
+++ src/app.config.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAXWTdNefM2SjR7Bo2I4zharZgF1-cW5_4",
+    authDomain: "slack-clone-936b3.firebaseapp.com",
+    projectId: "slack-clone-936b3",
+    storageBucket: "slack-clone-936b3.appspot.com",
+    messagingSenderId: "470822886370",
+    appId: "1:470822886370:web:7e1a84fd9170b2ae47b7bb",
+    measurementId: "G-LDEZDCPSBP"
+}
+
+const cometChat = {
+  APP_ID: '195941b61852b3b0',
+  AUTH_KEY: 'a367e1365df324c282b8489e62238974ee2160be',
+  REST_KEY: '3d2e7555cfa5ecc822da4ce4c3ccbd3760d978c8',
+  APP_REGION: 'us',
+}
+
+export { firebaseConfig, cometChat }
